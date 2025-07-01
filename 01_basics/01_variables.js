f4828217c1f959c9ab02c499e18295c51af54c6a
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-01 10:14:32 +0000

    conatainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "vishal@google.com"
+var accountpassword = "2222"
+accountCity = "jaipur"
+let accountState;
+
+
+accountEmail = "vk@gtbit.com"
+accountpassword = "1111"
+accountCity = "bengluru"
+/*
+    not use var
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
