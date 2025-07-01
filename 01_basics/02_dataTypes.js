19a5a855efba22379b00960dd340ccf428b385f4
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-01 10:35:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) we are using node js not browser
+
+/*
+    number=> 2 to power 23
+    bigint
+    string=>" "
+    boolean => true/false
+    null => standalone value
+    undefined =>
+    symbol =>unique
+
+
+
+    object
+*/
+let name = "vishal"
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
