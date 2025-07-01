55225d4edda4a5a60606074ec17ae8286af9399e
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-01 11:17:24 +0000

    conversion operatin

---
+++ 01_basics/03_conope.js
@@ -0,0 +1,50 @@
+// let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valNumber = Number(score)
+// console.log(typeof valNumber)
+// console.log(valNumber)
+
+/* 
+    "33" => 33
+    "33abc" => NaN
+    true => 1; false => 0
+
+*/
+
+let loggedIn = " vishal "
+
+let booleanIsloggedIn = Boolean(loggedIn)
+// console.log(booleanIsloggedIn)
+
+/* 
+    1 => true; 0 => false
+    "" => false; " " => true
+    "vishal " => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+// ****************************** OPERATIONS ***************************//
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "vishal"
+let str2 = " kushwaha"
+let str3 = str1 + str2 
+// console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
