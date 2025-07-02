adcccf540e0c247addd393f110e399b3ccdbf908
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-02 10:02:53 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+const name = "vishal"
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+
+console.log(`my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('vishallll')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('s'));
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring)
+
+// const anotherstring = gameName.slice(3,0)
+// console.log(anotherstring)
+
+const newString = "   vishal    "
+console.log(newString);
+console.log(newString.trim());
+
+const url = 'https://vishal.com/vishal%20kushwaha'
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('vishal'))
