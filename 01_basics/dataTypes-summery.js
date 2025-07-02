da359d6de9e76dc57c632e89e397532ce0addc56
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-02 09:13:58 +0000

    dataTypes-summery

---
+++ 01_basics/dataTypes-summery.js
@@ -0,0 +1,40 @@
+/* Primitive datatypes
+
+    7 Types : sytring,Number,Boolean,null,undefined,symbol,BigInt
+
+*/
+const score = 100
+const scoreValue = 100.3
+
+const osLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 123456789n
+
+/*
+    Refrence(non primitive)
+
+    Array,Objects,Functions
+*/
+
+const heros = ["shaktiman","doga","Nagraj"];
+
+// in paranthesis these are object
+let myobj = {
+    name : "vishal",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
