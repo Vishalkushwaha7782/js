c77f71cf6fa79a338ae2db7012af8593ec2ce6f0
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-02 10:40:09 +0000

    number and maths

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.length);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const anotherNumber = 14.628
+// console.log(anotherNumber.toPrecision(2));
+
+const Numberss = 10000000
+// console.log(Numberss.toLocaleString('en-IN'))
+
+
+//     +++++++++++++++  maths  +++++++++++++++++++  //
+
+// console.log(Math)
+
+// console.log(Math.abs(-2))
+// console.log(Math.round(2.6))
+// console.log(Math.ceil(2.2))
+// console.log(Math.floor(2.9))
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1 )
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
