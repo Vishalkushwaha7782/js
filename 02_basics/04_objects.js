5bb5c2a7308cfb1de70894ebdfac4aab9ff8d32b
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-03 10:10:01 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,31 @@
+// const tinderuser = new Object()
+
+const tinderuser = {}
+
+
+tinderuser.id = "123c"
+tinderuser.name = "vishal"
+tinderuser.isLoggedIn = false
+
+// console.log(tinderuser);
+
+const regularuser = {
+    fullName : {
+        userfullName : {
+            firstname : "vishal",
+            lastname : "kushwaha"
+        }
+    }
+}
+
+// console.log(regularuser.fullName.userfullName.lastname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"b"}
+
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
