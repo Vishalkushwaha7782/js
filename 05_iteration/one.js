32b6d6258b1497e8af598bb11bc19297b0f00526
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-04 10:40:16 +0000

    LOOPS

---
+++ 05_iteration/one.js
@@ -0,0 +1,30 @@
+// For loop
+
+// console.log("vishal")
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+
+let myarray = ["flash","batman","superman"];
+
+for(let i = 0; i < myarray.length;i++){
+    // console.log(myarray[i]);
+}
+
+// break and continue 
+
+// ehile loop
+let index = 0
+while (index <= 10) {
+    // console.log(`value is ${index}`);
+    index = index + 2;
+}
+
+let score = 1
+
+do {
+    console.log(`score is: ${score}`);
+    score++;
+} while (score <=10);
