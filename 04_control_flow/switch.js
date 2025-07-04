4060a982d3b5aec91e7e741c554f0e96759bdee3
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2025-07-04 09:46:16 +0000

    SWITCH

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("enter valdi number")
+        break;
+}
